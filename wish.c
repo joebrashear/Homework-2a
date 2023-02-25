34940820a19b5e1eea687d0ecbf813d6ef904425
joebrashear <joebrashear@live.com>
2023-02-24 22:56:24 -0500

    Add debug

--- wish.c
+++ wish.c
@@ -1,11 +1,12 @@
 // Code by William Brashear 
 #define _GNU_SOURCE
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 
-int debug = 0;
+int debug = 1;
 
 typedef struct LinkedList
 {
@@ -13,24 +14,152 @@ typedef struct LinkedList
     struct LinkedList *next;
 } ListNode;
 
+void deallocate_List(ListNode *head) 
+{
+    ListNode *temp = NULL;
+
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->path);
+        free(temp);
+    }
+
+    return;
+}
+
+void free_array(char **str_arr)
+{
+    int i = 0;
+    int len = sizeof(str_arr);
+
+    for (i = 0; i < len; i++)
+    {
+        free(str_arr[i]);
+    }
+    free(str_arr);
+}
+
+void print_error()
+{
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message));
+}
+
+ListNode *add_path_to_list(ListNode *head, char* new_path, size_t len)
+{
+    ListNode *temp = NULL;
+    
+    temp = head;
+    while (head->next != NULL)
+    {
+        head = head->next;
+    }
+
+    head->next = (ListNode *)calloc(1, sizeof(ListNode));
+    if (head->next == NULL)
+    {
+        print_error();
+        deallocate_List(temp);
+        return NULL;
+    }
+
+    head->next->path = (char *)calloc(len, sizeof(char));
+    if (head->next->path == NULL)
+    {
+        print_error();
+        deallocate_List(temp);
+        return NULL;
+    }
+
+    strcpy(head->next->path, new_path);
+
+    return temp;
+}
+
+char **get_args(char *arguments)
+{
+    char **args = NULL;
+    char *arg = NULL;
+    int size = 0;
+
+    while ((arg = strsep(&arguments, " ")) != NULL)
+    {
+        size++;
+        args = realloc(args, size);
+        if (args == NULL)
+        {
+            print_error();
+            free_array(args);
+            return NULL;
+        }
+        
+        args[size-1] = (char *)calloc(strlen(arg) + 1, sizeof(char));
+        if (args[size-1] == NULL)
+        {
+            print_error();
+            free_array(args);
+            return NULL;
+        }
+        strcpy(args[size-1], arg);
+    }
+    size++;
+    args = realloc(args, size);
+    if (args == NULL)
+    {
+        print_error();
+        free_array(args);
+        return NULL;
+    }
+
+    if (debug)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            
+            if (args[i] == NULL)
+            {
+                printf("NULL\n");
+            }
+            else{
+                printf("%s,", args[i]);
+            }
+        }
+        printf("%d\n", size);
+    }
+    
+    return args;
+}
+
 int process_args(char **args, ListNode *paths)
 {
     int len = sizeof(args);
     int i = 0;
-    char *arg = args[0];
+    char *arg = NULL;
+    ListNode *temp = NULL;
 
-    switch(arg)
+    while (i < len)
     {
-        case "cd":
-            break;
-        case "path":
-            break;
-        case "exit":
+        arg = args[i];
+        if (strcmp("cd", arg) == 0)
+        {
+            continue;
+        }
+        else if (strcmp("path", arg) == 0)
+        {
+            continue;
+        }
+        else if(strcmp("exit", arg) == 0)
+        {
             free_array(args);
             deallocate_List(paths);
             exit(0);
-        default:
+        }
+        else
+        {
             temp = paths;
+
             while (paths != NULL && access(paths->path, X_OK) == -1)
             {
                 paths = paths->next;
@@ -59,119 +188,10 @@ int process_args(char **args, ListNode *paths)
             }
 
             paths = temp;
+            return 2;
+        }
     }
     return 2;
-}
-
-void free_array(char **str_arr, int len)
-{
-    int i = 0;
-
-    for (i = 0; i < len; i++)
-    {
-        free(str_arr[i]);
-    }
-    free(str_arr);
-}
-
-char **get_args(char *arguments)
-{
-    char **args = NULL;
-    char *arg = NULL;
-    int size = 0;
-
-    while ((arg = strsep(&line, " ")) != NULL)
-    {
-        size++;
-        args = realloc(args, size);
-        if (args == NULL)
-        {
-            print_error();
-            free_array(args, size - 1);
-            return NULL;
-        }
-        
-        args[size-1] = (char *)calloc(strlen(arg) + 1, sizeof(char));
-        if (args[size-1] == NULL)
-        {
-            print_error();
-            free_array(args, size)
-            return NULL;
-        }
-        strcpy(args[size-1], arg);
-    }
-    return args;
-}
-
-void deallocate_List(ListNode *head) 
-{
-    ListNode *temp = NULL;
-
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp->command);
-        free(temp);
-    }
-
-    return;
-}
-
-int add_path_to_list(ListNode *head, char* new_path, size_t len)
-{
-    ListNode *new_list = NULL;
-    ListNode *temp = NULL;
-    
-    if (head == NULL)
-    {
-
-        ListNode *new_list = (ListNode *)calloc(1, sizeof(ListNode));
-        if (new_list == NULL)
-        {
-            print_error();
-            return 1;
-        }
-
-        new_list->path = (char *)calloc(len, sizeof(char));
-        if (new_list->path == NULL)
-        {
-            print_error():
-            free(new_list);
-            return 1;
-        }
-
-        strcpy(new_list->path, new_path);
-        head = new_list;
-    } 
-    else
-    {
-        temp = head;
-        while (head->next != NULL)
-        {
-            head = head->next;
-        }
-
-        head->next = (ListNode *)calloc(1, sizeof(ListNode));
-        if (head->next == NULL)
-        {
-            print_error();
-            deallocate_List(temp);
-            return 1;
-        }
-
-        head->next->path = (char *)calloc(len, sizeof(char));
-        if (head->next->path == NULL)
-        {
-            print_error():
-            deallocate_List(temp);
-            return 1;
-        }
-
-        strcpy(head->next->path, new_path);
-        head = temp;
-    }
-    return 0;
 }
 
 int run_wish(FILE *input, int is_interactive)
@@ -179,17 +199,17 @@ int run_wish(FILE *input, int is_interactive)
     char *line = NULL;
     size_t len = 0;
     int has_input = 0;
-    char *arg = NULL;
-    ListNode *paths = NULL;
-    ListNode *temp = NULL;
+    ListNode *paths = (ListNode *)calloc(1, sizeof(ListNode));
     char **args = NULL;
-    int ret = 0;
 
-    ret = add_path_to_list(paths, "/bin", strlen("/bin") + 1);
-    if (ret == 1)
+    paths = add_path_to_list(paths, "/bin", strlen("/bin") + 1);
+    if (paths == NULL)
     {
         exit(1);
     }
+
+    // ignore dummy head
+    paths = paths->next;
 
     while(1)
     {
@@ -216,12 +236,6 @@ int run_wish(FILE *input, int is_interactive)
         }
     }
     return 0;
-}
-
-void print_error()
-{
-    char error_message[30] = "An error has occurred\n";
-    write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
 int main(int argc, char *argv[])
@@ -254,6 +268,7 @@ int main(int argc, char *argv[])
 
                 if(run_wish(input, is_interactive) == 1)
                 {
+                    fclose(input);
                     exit(1);
                 }
                 fclose(input);
